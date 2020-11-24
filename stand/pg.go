d3c6f7fda3e56399a9b41c6adff27d546c489630
Evan Leis <evan.explodes@gmail.com>
2020-11-24 12:55:55 -0700

    [tokyo] db conn

--- stand/pg.go
+++ stand/pg.go
@@ -4,9 +4,9 @@ import "github.com/go-pg/pg/v10"
 
 func ConnectPostgres() *pg.DB {
 	return pg.Connect(&pg.Options{
-		Addr:     DefaultEnv("POSTGRES_HOST", "postgres"),
-		Database: RequireEnv("POSTGRES_DATABASE"),
-		Password: RequireEnv("POSTGRES_PASSWORD"),
-		User:     RequireEnv("POSTGRES_USER"),
+		Addr:     DefaultEnv("DB_HOST", "postgres"),
+		Database: RequireEnv("DB_DATABASE"),
+		Password: RequireEnv("DB_PASSWORD"),
+		User:     RequireEnv("DB_USER"),
 	})
 }
